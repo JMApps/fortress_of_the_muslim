745c790a16273af4afc83aa1d9d4b4e6d2ef00cc
jmapps <jmappsmuslim@gmail.com>
2021-10-18 09:12:33 +0300

    Add save settings value provider methods

---
+++ lib/constants/constants.dart
@@ -0,0 +1,6 @@
+final keyTextSizeValue = 'key_text_size_value';
+final keyArabicTextColorValue = 'key_arabic_text_color_value';
+final keyTranscriptionTextColorValue = 'key_transcription_text_color_value';
+final keyTranslationTextColorValue = 'key_translation_text_color_value';
+final keyArabicTextShow = 'key_arabic_text_show';
+final keyTranscriptionTextShow = 'key_transcription_text_show';
