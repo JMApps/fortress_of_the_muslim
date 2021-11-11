792118b4c88e3571e37ee805e0bb96357cb4eb0a
jmapps <jmappsmuslim@gmail.com>
2021-11-11 13:30:15 +0200

    Added draggable fab with new db

--- lib/widget/floating_counter_button.dart
+++ lib/widget/floating_counter_button.dart
@@ -1,3 +1,4 @@
+import 'package:draggable_fab/draggable_fab.dart';
 import 'package:flutter/material.dart';
 import 'package:fortress_of_the_muslim/provider/floating_counter_state.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
@@ -8,26 +9,28 @@ class FloatingCounterButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      child: FloatingActionButton(
-        backgroundColor: Colors.blueGrey,
-        splashColor: Colors.blueGrey[700],
-        child: CircularPercentIndicator(
-          animationDuration: 0,
-          radius: 55,
-          lineWidth: 2,
-          animation: true,
-          percent: context.watch<FloatingCounterState>().getCount / 100,
-          center: Text(
-            context.watch<FloatingCounterState>().getCount.toString(),
-            style: const TextStyle(color: Colors.white, fontSize: 18),
+    return GestureDetector(
+      child: DraggableFab(
+        child: FloatingActionButton(
+          backgroundColor: Colors.blueGrey,
+          splashColor: Colors.blueGrey[700],
+          child: CircularPercentIndicator(
+            animationDuration: 0,
+            radius: 55,
+            lineWidth: 2,
+            animation: true,
+            percent: context.watch<FloatingCounterState>().getCount / 100,
+            center: Text(
+              context.watch<FloatingCounterState>().getCount.toString(),
+              style: const TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            circularStrokeCap: CircularStrokeCap.round,
+            progressColor: Colors.blueGrey[900],
           ),
-          circularStrokeCap: CircularStrokeCap.round,
-          progressColor: Colors.blueGrey[900],
+          onPressed: () {
+            context.read<FloatingCounterState>().updateCountValue();
+          },
         ),
-        onPressed: () {
-          context.read<FloatingCounterState>().updateCountValue();
-        },
       ),
       onLongPress: () {
         context.read<FloatingCounterState>().setCount();
