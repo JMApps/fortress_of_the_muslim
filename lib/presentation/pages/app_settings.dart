e8fbc59631082988110deec61260a9dfd5604274
jmapps <jmappsmuslim@gmail.com>
2022-06-02 15:16:52 +0300

    Added switch list tiles for setting's

--- lib/presentation/pages/app_settings.dart
+++ lib/presentation/pages/app_settings.dart
@@ -16,7 +16,81 @@ class _AppSettingsState extends State<AppSettings> {
         preferredSize: Size(double.maxFinite, 50),
         child: SettingsAppBar(),
       ),
-      body: Container(),
+      body: ListView(
+        children: [
+          SwitchListTile.adaptive(
+            activeColor: const Color(0xff3f968c),
+            title: const Text(
+              'Список глав',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            subtitle: const Text(
+              'Открывать приложение со списка глав',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            onChanged: (value) {},
+            value: false,
+          ),
+          const Divider(indent: 16, endIndent: 16),
+          SwitchListTile.adaptive(
+            activeColor: const Color(0xff3f968c),
+            title: const Text(
+              'Последняя глава',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            subtitle: const Text(
+              'Запоминать последнюю читаемую главу',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            onChanged: (value) {},
+            value: true,
+          ),
+          const Divider(indent: 16, endIndent: 16),
+          SwitchListTile.adaptive(
+            activeColor: const Color(0xff3f968c),
+            title: const Text(
+              'Уведомления',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            subtitle: const Text(
+              'Ежедневные уведомления утром и вечером',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            onChanged: (value) {},
+            value: true,
+          ),
+          const Divider(indent: 16, endIndent: 16),
+          SwitchListTile.adaptive(
+            activeColor: const Color(0xff3f968c),
+            title: const Text(
+              'Экран',
+              style: TextStyle(
+                fontSize: 18,
+              ),
+            ),
+            subtitle: const Text(
+              'Оставить экран включенным пока приложение активно',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            onChanged: (value) {},
+            value: false,
+          ),
+        ],
+      ),
     );
   }
 }
