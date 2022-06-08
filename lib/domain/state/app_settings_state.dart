dc4ae1120430e80459a78b649bad18f22a65e754
jmapps <jmappsmuslim@gmail.com>
2022-06-08 21:34:39 +0300

    Added save and load state for main setting's

--- lib/domain/state/app_settings_state.dart
+++ lib/domain/state/app_settings_state.dart
@@ -6,6 +6,10 @@ class AppSettingsState with ChangeNotifier {
 
   bool get isDarkMode => themeMode == ThemeMode.dark;
 
+  bool isChapterMain = false;
+
+  bool get getIsChapterMain => isChapterMain;
+
   toggleTheme(bool isActive) async {
     themeMode = isActive ? ThemeMode.dark : ThemeMode.light;
     final preferences = await SharedPreferences.getInstance();
@@ -13,10 +17,19 @@ class AppSettingsState with ChangeNotifier {
     notifyListeners();
   }
 
-  useLatThemeState() async {
+  ifChaptersFirst(bool state) async {
+    isChapterMain = state;
+    final preferences = await SharedPreferences.getInstance();
+    preferences.setBool('key_is_chapters_first', state);
+    notifyListeners();
+  }
+
+  loadLastPreferencesState() async {
     final preferences = await SharedPreferences.getInstance();
     final isDarkMode = preferences.getBool('key_dark_theme') ?? false;
+    isChapterMain = preferences.getBool('key_is_chapters_first') ?? false;
     themeMode = isDarkMode ? ThemeMode.dark : ThemeMode.light;
     notifyListeners();
   }
+
 }
--- lib/presentation/pages/app_settings.dart
+++ lib/presentation/pages/app_settings.dart
@@ -15,112 +15,118 @@ class _AppSettingsState extends State<AppSettings> {
   @override
   Widget build(BuildContext context) {
     final myColor = Theme.of(context).colorScheme;
-    return Scaffold(
-      appBar: const PreferredSize(
-        preferredSize: Size(double.maxFinite, 50),
-        child: SettingsAppBar(),
-      ),
-      body: ListView(
-        physics: const BouncingScrollPhysics(),
-        padding: const EdgeInsets.all(8),
-        children: [
-          SwitchListTile.adaptive(
-            activeColor: myColor.mainSettingsColor,
-            title: const Text(
-              'Список глав',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: const Text(
-              'Открывать приложение со списка глав',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            onChanged: (value) {},
-            value: false,
+    return Consumer<AppSettingsState>(
+      builder: (BuildContext context, appSettings, Widget? child) {
+        return Scaffold(
+          appBar: const PreferredSize(
+            preferredSize: Size(double.maxFinite, 50),
+            child: SettingsAppBar(),
           ),
-          const Divider(indent: 16, endIndent: 16),
-          SwitchListTile.adaptive(
-            activeColor: myColor.mainSettingsColor,
-            title: const Text(
-              'Последняя глава',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
+          body: ListView(
+            physics: const BouncingScrollPhysics(),
+            padding: const EdgeInsets.all(8),
+            children: [
+              SwitchListTile.adaptive(
+                activeColor: myColor.mainSettingsColor,
+                title: const Text(
+                  'Список глав',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: const Text(
+                  'Открывать приложение со списка глав',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                value: appSettings.isChapterMain,
+                onChanged: (value) {
+                  appSettings.ifChaptersFirst(value);
+                },
               ),
-            ),
-            subtitle: const Text(
-              'Запоминать последнюю читаемую главу',
-              style: TextStyle(
-                fontSize: 16,
+              const Divider(indent: 16, endIndent: 16),
+              SwitchListTile.adaptive(
+                activeColor: myColor.mainSettingsColor,
+                title: const Text(
+                  'Последняя глава',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: const Text(
+                  'Запоминать последнюю читаемую главу',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                onChanged: (value) {},
+                value: true,
               ),
-            ),
-            onChanged: (value) {},
-            value: true,
+              const Divider(indent: 16, endIndent: 16),
+              SwitchListTile.adaptive(
+                activeColor: myColor.mainSettingsColor,
+                title: const Text(
+                  'Уведомления',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: const Text(
+                  'Ежедневные уведомления утром и вечером',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                onChanged: (value) {},
+                value: true,
+              ),
+              const Divider(indent: 16, endIndent: 16),
+              SwitchListTile.adaptive(
+                activeColor: myColor.mainSettingsColor,
+                title: const Text(
+                  'Экран',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: const Text(
+                  'Оставить экран включенным пока приложение активно',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                onChanged: (value) {},
+                value: false,
+              ),
+              SwitchListTile.adaptive(
+                activeColor: myColor.mainSettingsColor,
+                title: const Text(
+                  'Тема',
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                subtitle: const Text(
+                  'Текущая тема (адаптивная)',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                value: context.watch<AppSettingsState>().isDarkMode,
+                onChanged: (value) {
+                  context.read<AppSettingsState>().toggleTheme(value);
+                },
+              ),
+            ],
           ),
-          const Divider(indent: 16, endIndent: 16),
-          SwitchListTile.adaptive(
-            activeColor: myColor.mainSettingsColor,
-            title: const Text(
-              'Уведомления',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: const Text(
-              'Ежедневные уведомления утром и вечером',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            onChanged: (value) {},
-            value: true,
-          ),
-          const Divider(indent: 16, endIndent: 16),
-          SwitchListTile.adaptive(
-            activeColor: myColor.mainSettingsColor,
-            title: const Text(
-              'Экран',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: const Text(
-              'Оставить экран включенным пока приложение активно',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            onChanged: (value) {},
-            value: false,
-          ),
-          SwitchListTile.adaptive(
-            activeColor: myColor.mainSettingsColor,
-            title: const Text(
-              'Тема',
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            subtitle: const Text(
-              'Текущая тема (адаптивная)',
-              style: TextStyle(
-                fontSize: 16,
-              ),
-            ),
-            value: context.watch<AppSettingsState>().isDarkMode,
-            onChanged: (value) {
-              context.read<AppSettingsState>().toggleTheme(value);
-            },
-          ),
-        ],
-      ),
+        );
+      },
     );
   }
 }
