11e6a267bcb5f380b22350b022b62d6c15f3b9e5
jmapps <jmappsmuslim@gmail.com>
2022-07-04 07:17:30 +0300

    Fixes url launch for stores

--- lib/presentation/items/main_item.dart
+++ lib/presentation/items/main_item.dart
@@ -32,11 +32,11 @@ class MainItem extends StatelessWidget {
         borderRadius: BorderRadius.circular(15),
         onTap: () async {
           if (route == 'app_ios_account') {
-            if (Platform.isIOS) {
-              await launchUrl(_urlIOS);
-            } else if (Platform.isAndroid) {
-              await launchUrl(_urlAndroid);
-            }
+            await launchUrl(
+              Platform.isIOS
+                  ? _urlIOS
+                  : _urlAndroid,
+            );
           } else {
             Navigator.pushNamed(context, route);
           }
