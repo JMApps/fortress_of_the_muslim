d98458ac59fd5b89fe936a1fea8f6ba46eaf6294
jmapps <jmappsmuslim@gmail.com>
2022-07-19 23:20:08 +0300

    Changed status bar color for android version's to color transparent

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:fortress_of_the_muslim/application/main_app.dart';
 import 'package:fortress_of_the_muslim/data/constants.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
   await Hive.initFlutter();
   await Hive.openBox(Constants.keyMainSettingBox);
   runApp(const MainApp());
