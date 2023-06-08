6cbf5621725f2a7c6005f1d13d656146778624e1
dev_muslim <jmappsmuslim@gmail.com>
2023-06-08 21:57:36 +0300

    Changes strings and added new DB

--- lib/application/theme/app_themes.dart
+++ lib/application/theme/app_themes.dart
@@ -11,7 +11,7 @@ class AppThemes {
       centerTitle: true,
       elevation: 0,
       shape: AppStyles.appBarShape,
-      backgroundColor: Color(0xFF00796B),
+      backgroundColor: Color(0xFF009688),
       titleTextStyle: TextStyle(
         color: Color(0xFFFFFFFF),
         fontFamily: 'Gilroy',
@@ -28,7 +28,7 @@ class AppThemes {
         fontSize: 17,
         fontFamily: 'Gilroy',
         fontWeight: FontWeight.bold,
-        color: Color(0xFF00796B),
+        color: Color(0xFF009688),
       ),
       bodyMedium: TextStyle(
         fontSize: 17,
@@ -46,13 +46,13 @@ class AppThemes {
   static final darkTheme = ThemeData(
     brightness: Brightness.dark,
     primarySwatch: Colors.blueGrey,
-    scaffoldBackgroundColor: const Color(0xFF263238),
+    scaffoldBackgroundColor: const Color(0xFF192326),
     fontFamily: 'Gilroy',
     appBarTheme: const AppBarTheme(
       centerTitle: true,
       elevation: 0,
       shape: AppStyles.appBarShape,
-      backgroundColor: Color(0xFF192326),
+      backgroundColor: Color(0xFF243336),
       titleTextStyle: TextStyle(
         color: Color(0xFFFFFFFF),
         fontFamily: 'Gilroy',
@@ -86,35 +86,51 @@ class AppThemes {
 }
 
 extension ColorsSchemes on ColorScheme {
+  Color get primaryDefaultColor => brightness == Brightness.light
+      ? const Color(0xFF009688)
+      : const Color(0xFF4DB6AC);
+
   Color get mainChaptersColor => brightness == Brightness.light
-      ? const Color(0xBF00897B)
+      ? const Color(0xFF009688)
       : const Color(0xFF00443E);
+
+  Color get mainChaptersBackgroundColor => brightness == Brightness.light
+      ? const Color(0xFFE0F2F1)
+      : const Color(0xFF192326);
 
   Color get mainBookmarksColor => brightness == Brightness.light
       ? const Color(0xBFFB8C00)
       : const Color(0xFF7A2A00);
 
+  Color get mainBookmarksBackgroundColor => brightness == Brightness.light
+      ? const Color(0xFFFFF3E0)
+      : const Color(0xFF192326);
+
   Color get mainSupplicationsColor => brightness == Brightness.light
       ? const Color(0xBFE53935)
       : const Color(0xFF671616);
 
+  Color get mainSupplicationsBackgroundColor => brightness == Brightness.light
+      ? const Color(0xFFFFEBEE)
+      : const Color(0xFF192326);
+
   Color get mainFavoritesColor => brightness == Brightness.light
       ? const Color(0xBF1E88E5)
       : const Color(0xFF002054);
+
+  Color get mainFavoritesBackgroundColor => brightness == Brightness.light
+      ? const Color(0xFFE1F5FE)
+      : const Color(0xFF192326);
 
   Color get mainDefaultColor => brightness == Brightness.light
       ? const Color(0xFF1C1C1C)
       : const Color(0xFFD3D3D3);
 
   Color get cardOddColor => brightness == Brightness.light
-      ? const Color(0xFFE0F2F1)
+      ? const Color(0xFFF5F5F5)
       : const Color(0xFF263238);
 
   Color get cardColor => brightness == Brightness.light
       ? const Color(0xFFFFFFFF)
       : const Color(0xFF192326);
-
-  Color get primaryDefaultColor => brightness == Brightness.light
-      ? const Color(0xBF00897B)
-      : const Color(0xFF4DB6AC);
 }
