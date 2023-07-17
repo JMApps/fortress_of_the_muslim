892cd4763ed97b0cb59f23fa9c4b0f1a6128d417
dev_muslim <jmappsmuslim@gmail.com>
2023-07-17 21:26:42 +0300

    Added app player

--- lib/presentation/content/chapter_content_page.dart
+++ lib/presentation/content/chapter_content_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fortress_of_the_muslim/application/state/app_player_state.dart';
 import 'package:fortress_of_the_muslim/application/state/main_chapters_state.dart';
 import 'package:fortress_of_the_muslim/application/string/app_strings.dart';
 import 'package:fortress_of_the_muslim/application/style/app_styles.dart';
@@ -18,69 +19,78 @@ class ChapterContentPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
-    return Scaffold(
-      backgroundColor: theme.colorScheme.chapterContentSupplicationsBackgroundColor,
-      body: FutureBuilder<List>(
-        future: context.watch<MainChaptersState>().getDatabaseQuery.getChapterContentSupplications(chapterId),
-        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-          return snapshot.hasData
-              ? CustomScrollView(
-                  slivers: [
-                    SliverAppBar(
-                      centerTitle: true,
-                      title: Text('${AppStrings.head} $chapterId'),
-                      floating: true,
-                      backgroundColor: theme.colorScheme.chapterContentSupplicationsColor,
-                    ),
-                    SliverToBoxAdapter(
-                      child: FutureBuilder<List>(
-                        future: context.read<MainChaptersState>().getDatabaseQuery.getOneChapter(chapterId),
-                        builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
-                          return snapshot.hasData
-                              ? Card(
-                                  margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
-                                  elevation: 0,
-                                  color: theme.colorScheme.chapterContentSupplicationsColor,
-                                  child: Padding(
-                                    padding: AppStyles.mainPadding,
-                                    child: ForHtmlText(
-                                      textData: snapshot.data?[0].chapterTitle,
-                                      textSize: 17,
-                                      textColor: Colors.white,
-                                      footnoteColor: theme.colorScheme.mainChaptersColor,
-                                      textDataAlign: TextAlign.center,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => AppPlayerState(),
+        ),
+      ],
+      child: Scaffold(
+        backgroundColor: theme.colorScheme.chapterContentSupplicationsBackgroundColor,
+        body: FutureBuilder<List>(
+          future: context.watch<MainChaptersState>().getDatabaseQuery.getChapterContentSupplications(chapterId),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+            return snapshot.hasData
+                ? CustomScrollView(
+                    slivers: [
+                      SliverAppBar(
+                        centerTitle: true,
+                        title: Text('${AppStrings.head} $chapterId'),
+                        floating: true,
+                        backgroundColor: theme.colorScheme.chapterContentSupplicationsColor,
+                      ),
+                      SliverToBoxAdapter(
+                        child: FutureBuilder<List>(
+                          future: context.read<MainChaptersState>().getDatabaseQuery.getOneChapter(chapterId),
+                          builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
+                            return snapshot.hasData
+                                ? Card(
+                                    margin: const EdgeInsets.fromLTRB(8, 8, 8, 0),
+                                    elevation: 0,
+                                    color: theme.colorScheme.chapterContentSupplicationsColor,
+                                    child: Padding(
+                                      padding: AppStyles.mainPadding,
+                                      child: ForHtmlText(
+                                        textData: snapshot.data?[0].chapterTitle,
+                                        textSize: 17,
+                                        textColor: Colors.white,
+                                        footnoteColor: theme.colorScheme.mainChaptersColor,
+                                        textDataAlign: TextAlign.center,
+                                      ),
                                     ),
-                                  ),
-                                )
-                              : const Card(
-                                  margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
-                                  elevation: 0,
-                                  child: Padding(
-                                    padding: AppStyles.mainPadding,
-                                    child: CircularProgressIndicator.adaptive(),
-                                  ),
-                                );
-                        },
+                                  )
+                                : const Card(
+                                    margin: EdgeInsets.fromLTRB(8, 8, 8, 0),
+                                    elevation: 0,
+                                    child: Padding(
+                                      padding: AppStyles.mainPadding,
+                                      child:
+                                          CircularProgressIndicator.adaptive(),
+                                    ),
+                                  );
+                          },
+                        ),
                       ),
-                    ),
-                    SliverList(
-                      delegate: SliverChildBuilderDelegate(
-                        (BuildContext context, int itemIndex) {
-                          return ContentChapterSupplicationItem(
-                            item: snapshot.data![itemIndex],
-                            itemIndex: itemIndex,
-                            itemsLength: snapshot.data!.length,
-                          );
-                        },
-                        childCount: snapshot.data!.length,
+                      SliverList(
+                        delegate: SliverChildBuilderDelegate(
+                          (BuildContext context, int itemIndex) {
+                            return ContentChapterSupplicationItem(
+                              item: snapshot.data![itemIndex],
+                              itemIndex: itemIndex,
+                              itemsLength: snapshot.data!.length,
+                            );
+                          },
+                          childCount: snapshot.data!.length,
+                        ),
                       ),
-                    ),
-                  ],
-                )
-              : const Center(
-                  child: CircularProgressIndicator.adaptive(),
-                );
-        },
+                    ],
+                  )
+                : const Center(
+                    child: CircularProgressIndicator.adaptive(),
+                  );
+          },
+        ),
       ),
     );
   }
