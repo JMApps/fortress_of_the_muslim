1f566a454745a1cc3d26a8ae64e9f636994ddd8a
jmapps <jmappsmuslim@gmail.com>
2024-08-31 12:28:26 +0300

    Added roboto font

--- lib/core/strings/app_strings.dart
+++ lib/core/strings/app_strings.dart
@@ -2,9 +2,13 @@ class AppStrings {
   static const String appName = 'Крепость мусульманина';
 
   static const String chapters = 'Главы';
+  static const String searchChapters = 'Поиск по главам';
   static const String favoriteChapters = 'Избранное';
+  static const String searchFavoriteChapters = 'Поиск избранных глав';
   static const String allSupplications = 'Все дуа';
+  static const String searchSupplications = 'Поиск дуа';
   static const String favoriteSupplications = 'Избранные дуа';
+  static const String searchFavoriteSupplications = 'Поиск избранных дуа';
 
   static const String morning = 'Утром';
   static const String evening = 'Вечером';
@@ -19,4 +23,5 @@ class AppStrings {
   static const String share = 'Поделиться';
 
   static const String fontRaleway = 'Raleway';
+  static const String fontRoboto = 'Roboto';
 }
