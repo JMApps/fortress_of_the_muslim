6f6c9dc0c8a4a88e13b82ea552fc857b4f72ef00
jmapps <jmappsmuslim@gmail.com>
2024-08-31 14:43:35 +0300

    Added providers to main class

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import 'data/states/footnotes_state.dart';
+import 'data/states/main_chapters_state.dart';
 import 'presentation/pages/root_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   runApp(
-    const RootPage(),
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => MainChaptersState(),
+        ),
+        ChangeNotifierProvider(
+          create: (_) => FootnotesState(),
+        ),
+      ],
+      child: const RootPage(),
+    ),
   );
 }
