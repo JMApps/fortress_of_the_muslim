b8fe65e6040541921de5b7c92aad30ddc1f91c00
jmapps <jmappsmuslim@gmail.com>
2024-10-08 17:38:30 +0300

    Added main description text

---
+++ lib/presentation/widgets/main_description.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import '../../core/styles/app_styles.dart';
+
+class MainDescriptionText extends StatelessWidget {
+  const MainDescriptionText({
+    super.key,
+    required this.descriptionText,
+  });
+
+  final String descriptionText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: AppStyles.padding,
+      alignment: Alignment.center,
+      child: Text(
+        descriptionText,
+        style: const TextStyle(
+          fontSize: 20,
+        ),
+      ),
+    );
+  }
+}
