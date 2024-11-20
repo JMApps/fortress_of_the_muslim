6a9ff73bc92f604ef084fafafd7a77157c288d89
jmapps <jmappsmuslim@gmail.com>
2024-11-20 16:43:36 +0300

    Added flutter localization

--- lib/presentation/pages/root_page.dart
+++ lib/presentation/pages/root_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import '../../core/routes/app_routes.dart';
+import '../../core/strings/app_constraints.dart';
 import '../../core/themes/app_themes.dart';
 import '../states/app_settings_state.dart';
 import 'main_page.dart';
@@ -17,6 +19,10 @@ class RootPage extends StatelessWidget {
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           onGenerateRoute: AppRoutes.onRouteGenerator,
+          localizationsDelegates: AppLocalizations.localizationsDelegates,
+          supportedLocales: AppLocalizations.supportedLocales,
+          locale: AppConstraints.appLocales[appSettings.getAppLocaleIndex],
+          onGenerateTitle: (context) => AppLocalizations.of(context)!.appName,
           theme: appThemes.lightTheme,
           darkTheme: appThemes.darkTheme,
           themeMode: appSettings.getThemeMode,
---
+++ lib/presentation/settings/widgets/app_locale_drop_down.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../../core/strings/app_constraints.dart';
+import '../../../core/styles/app_styles.dart';
+import '../../states/app_settings_state.dart';
+
+class AppLocaleDropDown extends StatelessWidget {
+  const AppLocaleDropDown({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final appLocale = AppLocalizations.of(context)!;
+    final appColors = Theme.of(context).colorScheme;
+    final itemSelectedTextStyle = TextStyle(fontSize: 16, fontFamily: AppConstraints.fontGilroy, color: appColors.primary, fontWeight: FontWeight.bold);
+    return Consumer<AppSettingsState>(
+      builder: (BuildContext context, appSettings, _) {
+        return ListTile(
+          visualDensity: VisualDensity.comfortable,
+          title: Text(
+            appLocale.language,
+            style: AppStyles.mainTextStyle17Bold,
+          ),
+          subtitle: Text(appLocale.appLanguage),
+          leading: Icon(Icons.language_rounded),
+          trailing: DropdownButton<int>(
+            iconEnabledColor: appColors.primary,
+            borderRadius: AppStyles.border,
+            padding: AppStyles.paddingHorizontalMini,
+            elevation: 1,
+            alignment: Alignment.centerRight,
+            value: appSettings.getAppLocaleIndex,
+            items: List.generate(
+              AppConstraints.appLanguages.length,
+              (index) => DropdownMenuItem<int>(
+                value: index,
+                child: Center(
+                  child: Padding(
+                    padding: AppStyles.paddingRightMini,
+                    child: Text(
+                      AppConstraints.appLanguages[index],
+                      style: appSettings.getAppLocaleIndex == index ? itemSelectedTextStyle : AppStyles.mainTextStyle17,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            onChanged: (newIndex) {
+              appSettings.setAppLocaleIndex = newIndex!;
+            },
+            underline: const SizedBox(),
+          ),
+        );
+      },
+    );
+  }
+}
