fecd9525c0fcfb12ab6d162321de253c58720d77
jmapps <jmappsmuslim@gmail.com>
2024-11-24 07:34:54 +0300

    Added for fonts

--- lib/presentation/pages/root_page.dart
+++ lib/presentation/pages/root_page.dart
@@ -15,7 +15,7 @@ class RootPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer<AppSettingsState>(
       builder: (context, appSettings, _) {
-        final appThemes = AppThemes(Color(appSettings.getAppThemeColor));
+        final appThemes = AppThemes(Color(appSettings.getAppThemeColor), AppConstraints.appLocales[appSettings.getAppLocaleIndex]);
         return MaterialApp(
           debugShowCheckedModeBanner: false,
           onGenerateRoute: AppRoutes.onRouteGenerator,
